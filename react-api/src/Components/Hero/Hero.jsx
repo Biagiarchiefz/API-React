0cce9b4e21f4a1830b886ddb2a832def150f60c7
Biagiarchiefz <biagiiarchie@gmail.com>
2024-11-15 00:26:05 +0700

    read

--- react-api/src/Components/Hero/Hero.jsx
+++ react-api/src/Components/Hero/Hero.jsx
@@ -1,22 +1,26 @@
 import coba from "../../assets/coba.jpeg";
 import { useState, useEffect } from "react";
 
-const Hero = () => {
-  // => fungsional components
+const Hero = () => {                                // => fungsional components
   const url = "https://fakestoreapi.com/products";
   const [product, setProducts] = useState([]); // => membuat state untuk menangani data yang ada di dalam functional component (semacam variabel yang menyimpan data)
-  // setProduct digunakan untuk mengubah isi dari product
+                                               // setProduct digunakan untuk mengubah isi dari product
   const getDataProduct = async () => {
-    const response = await fetch(url);
-    const dataProduct = await response.json();
+    const response = await fetch(url);              // => fetch(url) adalah cara untuk mengambil data dari api
+                                                   // await digunakan untuk menunggu hasil dari fetch. menunggu fetch sampai menerima data, ketika fetch sudah menerima data dia akan melanjutkan ke baris berikutnya
 
-    setProducts(dataProduct); // memasukan data yang kita ambil dari api ke dalam state (product)
+    const dataProduct = await response.json();    // => response.json() mengubah response dari API yang tadinya berbentuk teks mentah menjadi (objek JSON) yang bisa kita gunakan di JavaScript.
+                                                  // await di sini juga digunakan untuk menunggu sampai proses pengubahan ke JSON selesai.
+                                                  // dataProduct sekrang menyimpan data product yang kita ambil dari API
+
+    setProducts(dataProduct);         // memasukan data yang kita ambil dari api ke dalam state (product) melalui setProduct
+                                      // product sekarang berisi data produk terbaru yang kita ambil dari API.
     console.log(product);
   };
 
-  useEffect(() => {
-    getDataProduct();
-  }, []);   // => [] digunakan untuk menjalankan useeffect cuma sekali pada saat awal di render
+  useEffect(() => {     // () => { getDataProduct(); } adalah fungsi yang akan dijalankan oleh useEffect.
+    getDataProduct();   // Saat komponen pertama kali muncul di layar, useEffect langsung menjalankan getDataProduct().
+  }, []);   // => [] digunakan untuk menjalankan useeffect cuma sekali saja pada saat awal di render (bisa di pake bisa tidak)
 
   return (
     <div className="container grid grid-cols-3">
